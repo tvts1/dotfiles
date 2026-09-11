541c7d51983b991964422ec18d0323262a8e91c4
tassioT <tvts.dev@gmail.com>
2026-09-11 16:15:04 -0300

    chore: update nvim init config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,23 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local function transparent_bg()
+  local groups = {
+    "Normal",
+    "NormalNC",
+    "SignColumn",
+    "EndOfBuffer",
+    "NormalFloat",
+    "FloatBorder",
+  }
+
+  for _, group in ipairs(groups) do
+    vim.api.nvim_set_hl(0, group, { bg = "none" })
+  end
+end
+
+transparent_bg()
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = transparent_bg,
+})
